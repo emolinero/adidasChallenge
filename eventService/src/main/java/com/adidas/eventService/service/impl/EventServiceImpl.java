baeb78fba7872c1d21809919e5233d23fc726944
Damian Quiroga <damianaquiroga@gmail.com>
2019-04-03 11:20:52 -0300

    Send queue message as a json string

--- eventService/src/main/java/com/adidas/eventService/service/impl/EventServiceImpl.java
+++ eventService/src/main/java/com/adidas/eventService/service/impl/EventServiceImpl.java
@@ -1,14 +1,11 @@
 package com.adidas.eventService.service.impl;
 
-import com.adidas.eventService.EventApplication;
 import com.adidas.eventService.model.Event;
 import com.adidas.eventService.service.EventService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,9 +24,7 @@ public class EventServiceImpl implements EventService {
   @Override
   public Event createEvent(Event event) {
     JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
-
     logger.info("Sending an event");
-    System.out.println("Sending an event");
     jmsTemplate.convertAndSend("events", event);
     return event;
   }
---
+++ subscriptionService/src/main/java/com/adidas/subscriptions/dto/EventDto.java
@@ -0,0 +1,34 @@
+package com.adidas.subscriptions.dto;
+
+public class EventDto {
+  private Long id;
+  private String description;
+  private Long newsletterId;
+
+  public Long getId() {
+    return id;
+  }
+
+  public EventDto setId(Long id) {
+    id = id;
+    return this;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public EventDto setDescription(String description) {
+    this.description = description;
+    return this;
+  }
+
+  public Long getNewsletterId() {
+    return newsletterId;
+  }
+
+  public EventDto setNewsletterId(Long newsletterId) {
+    this.newsletterId = newsletterId;
+    return this;
+  }
+}
