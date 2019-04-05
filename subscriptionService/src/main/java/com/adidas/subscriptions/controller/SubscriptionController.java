b65f6d5f7a396b2bb8fdcf26040b1e563cf9ccc4
Damian Quiroga <damianaquiroga@gmail.com>
2019-04-05 00:39:16 -0300

    Add integration tests

--- subscriptionService/src/main/java/com/adidas/subscriptions/controller/SubscriptionController.java
+++ subscriptionService/src/main/java/com/adidas/subscriptions/controller/SubscriptionController.java
@@ -8,8 +8,6 @@ import com.adidas.subscriptions.service.SubscriptionService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import ma.glasnost.orika.MapperFacade;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
