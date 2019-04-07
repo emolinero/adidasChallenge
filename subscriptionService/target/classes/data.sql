843ca9480edb87e001d67feb32fad692ac676132
Ezequiel Molinero <emolinero@gmail.com>
2019-04-07 16:53:18 -0300

    Delete target files

--- subscriptionService/target/classes/data.sql
+++
@@ -1,9 +0,0 @@
-insert into newsletters (ID,NAME,DESCRIPTION) values (1,'Shirts','Shirts Campaign');
-insert into newsletters (ID,NAME,DESCRIPTION) values (2,'Shoes','Shoes Campaign');
-insert into newsletters (ID,NAME,DESCRIPTION) values (3,'Jackets','Jackets Campaign');
-insert into newsletters (ID,NAME,DESCRIPTION) values (4,'Bottoms','Bottoms Campaign');
-insert into newsletters (ID,NAME,DESCRIPTION) values (5,'Accesories','Accesories Campaign');
-
--- Create s default user, that allow make writes operations
-
-INSERT INTO USERS (ID, USERNAME, PASSWORD) VALUES ( 1, 'demo', '$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.');
